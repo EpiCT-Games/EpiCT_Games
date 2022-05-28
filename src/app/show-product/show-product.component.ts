5a3bc643fda9e1d7c85b2467a646d00582c299b5
Cláudio <claudioasensio@ua.pt>
2022-05-29 00:41:55 +0100

    Show Product almost done

--- src/app/show-product/show-product.component.ts
+++ src/app/show-product/show-product.component.ts
@@ -8,20 +8,31 @@ import { product, SharedService } from '../shared.service';
   styleUrls: ['./show-product.component.scss']
 })
 export class ShowProductComponent implements OnInit {
-  product: product | null = null;
-  subscription: Subscription = new Subscription();
+  product : product | null = null;
+  subscription : Subscription = new Subscription();
+  rateInt : number[] = [];
+  rateDouble : number = 0;
   
   constructor(private _service: SharedService) {
     this.subscription = this._service.productOpened.subscribe((data: product) => {
       this.product = data;
     });
+    
   }
 
   ngOnInit(): void {
     console.log(this.product);
+    this.calculateRate();
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+  
+  calculateRate(){
+    this.rateInt = Array(Math.floor(Number(this.product?.rating))).fill(Math.floor(Number(this.product?.rating))); // [4,4,4,4,4]
+    console.log(this.rateInt);
+    this.rateDouble = Number(this.product?.rating) - this.rateInt.length;
+    console.log(this.rateDouble);
   }
 }
